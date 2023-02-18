4f2185f0b8be16f6f0a791ec608c933d5be84dfe
atompi <atompi@foxmail.com>
2023-02-18 12:44:52 +0800

    feat: add 3 solutions

---
+++ eightfourfour/eightfourfour.go
@@ -0,0 +1,23 @@
+package eightfourfour
+
+func BackspaceCompare(s string, t string) bool {
+	bcS := backspaceString(s)
+	bcT := backspaceString(t)
+	return bcS == bcT
+}
+
+func backspaceString(s string) string {
+	rawStack := []byte{}
+	for i := 0; i < len(s); i++ {
+		if s[i] != '#' {
+			rawStack = append(rawStack, s[i])
+		} else {
+			if len(rawStack)-1 >= 0 {
+				rawStack = rawStack[0 : len(rawStack)-1]
+			} else {
+				continue
+			}
+		}
+	}
+	return string(rawStack)
+}
