3f5a5c54f20806f8e50d04d8340bb73878d0f405
atompi <atompi@foxmail.com>
2023-04-03 09:52:16 +0800

    feat: add solution to lc881

--- main.go
+++ main.go
@@ -3,25 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/atompi/leetcode/problemset/lc2609"
+	"github.com/atompi/leetcode/problemset/lc881"
 )
 
 func main() {
-	// 2568 out of memory
-	// nums := []int{8388608, 131072, 128, 2097152, 65536, 2048, 438, 1048576, 8192, 32, 8, 64, 1024, 2244, 512, 262144, 4096, 16384, 4, 256, 2, 4194304, 2203, 16, 32768, 410, 524288, 765, 1}
-	// result := twofivesixeight.MinUnex(nums)
-
-	// 2569 wrong answer
-	// nums1 := []int{1, 0, 1}
-	// nums2 := []int{0, 0, 0}
-	// queries := [][]int{{1, 1, 1}, {2, 1, 0}, {3, 0, 0}}
-	// result := twofivesixnine.AnswerQueries(nums1, nums2, queries)
-
-	// 2567 wrong answer
-	// nums := []int{1, 4, 3}
-	// result := twofivesixseven.MinimumScore(nums)
-
-	s := "000111"
-	result := lc2609.FindTheLongestBalancedSubstring(s)
+	people := []int{5, 1, 2, 4}
+	limit := 6
+	result := lc881.NumRescueBoats(people, limit)
 	fmt.Println(result)
 }
