6347757d7f14af4e4821b87a2e0caa87950167ce
atompi <atompi@foxmail.com>
2023-04-12 23:22:00 +0800

    feat: add solution to lc71

---
+++ problemset/lc71/lc71.go
@@ -0,0 +1,27 @@
+package lc71
+
+import (
+	"path/filepath"
+	"strings"
+)
+
+func SimplifyPath(path string) string {
+	var stk []string
+	for _, s := range strings.Split(path, "/") {
+		if s == "" || s == "." {
+			continue
+		}
+		if s == ".." {
+			if len(stk) > 0 {
+				stk = stk[0 : len(stk)-1]
+			}
+		} else {
+			stk = append(stk, s)
+		}
+	}
+	return "/" + strings.Join(stk, "/")
+}
+
+func SimplifyPath2(path string) string {
+	return filepath.Clean(path)
+}
