b6778da28a146e84864c46b0533aca9f74b33093
atompi <coder.atompi@gmail.com>
2025-02-14 12:30:19 +0800

    feat: add more solutions

---
+++ problemset/lc740/lc740.go
@@ -0,0 +1,22 @@
+package lc740
+
+import "math"
+
+func DeleteAndEarn(nums []int) int {
+	mx := math.MinInt32
+	for _, num := range nums {
+		mx = max(mx, num)
+	}
+	total := make([]int, mx+1)
+	for _, num := range nums {
+		total[num] += num
+	}
+	first := total[0]
+	second := max(total[0], total[1])
+	for i := 2; i <= mx; i++ {
+		cur := max(first+total[i], second)
+		first = second
+		second = cur
+	}
+	return second
+}
--- problemset/lc746/lc746.go
+++ problemset/lc746/lc746.go
@@ -9,10 +9,3 @@ func MinCostClimbingStairs(cost []int) int {
 	}
 	return min(dp0, dp1)
 }
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
